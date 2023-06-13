d8d1d56de0525e835110e81422a61851ef770086
supremed3v <saadsiddiqui-programmer@protonmail.com>
2023-06-13 15:20:19 +0500

    working on folder structure

---
+++ frontend/src/renderer/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Electron Vite App</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
